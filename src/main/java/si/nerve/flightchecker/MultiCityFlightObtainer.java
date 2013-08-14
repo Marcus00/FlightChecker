0327a453853184e31821f2c5bd49b65dd5db331e
bratwurzt <johnny.galatikitis@gmail.com>
2013-08-14 23:54:02 +0200

    - hashcode/equals fix

--- src/main/java/si/nerve/flightchecker/MultiCityFlightObtainer.java
+++ src/main/java/si/nerve/flightchecker/MultiCityFlightObtainer.java
@@ -1,13 +1,15 @@
 package si.nerve.flightchecker;
 
 import java.io.IOException;
-import java.net.UnknownHostException;
 import java.util.Date;
+import java.util.Set;
+
+import si.nerve.flightchecker.data.MultiCityFlightData;
 
 /**
  * Created: 10.8.13 20:27
  */
 public interface MultiCityFlightObtainer
 {
-  public MultiCityFlightData get(String from1, String to1, Date date1, String from2, String to2, Date date2) throws IOException;
+  public Set<MultiCityFlightData> get(String from1, String to1, Date date1, String from2, String to2, Date date2) throws IOException;
 }
