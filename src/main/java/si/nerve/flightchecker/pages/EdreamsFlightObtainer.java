25f8a29a21706b94f8a915a182b073fb3ec3ac61
bratwurzt <johnny.galatikitis@gmail.com>
2013-09-23 16:09:40 +0200

    - edreams working demo

--- src/main/java/si/nerve/flightchecker/pages/EdreamsFlightObtainer.java
+++ src/main/java/si/nerve/flightchecker/pages/EdreamsFlightObtainer.java
@@ -1,35 +1,32 @@
 package si.nerve.flightchecker.pages;
 
-import java.awt.Color;
+import org.apache.log4j.Logger;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.nodes.Node;
+import org.jsoup.nodes.TextNode;
+import org.jsoup.select.Elements;
+import si.nerve.flightchecker.FlightsGui;
+import si.nerve.flightchecker.components.MultiCityFlightTableModel;
+import si.nerve.flightchecker.data.FlightLeg;
+import si.nerve.flightchecker.data.MultiCityFlightData;
+import si.nerve.flightchecker.helper.Helper;
+
+import javax.swing.*;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
 import java.net.URLEncoder;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 import java.util.zip.GZIPInputStream;
-import javax.swing.JLabel;
-
-import org.apache.log4j.Logger;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.JSONValue;
-import si.nerve.flightchecker.FlightsGui;
-import si.nerve.flightchecker.components.MultiCityFlightTableModel;
-import si.nerve.flightchecker.data.FlightLeg;
-import si.nerve.flightchecker.data.MultiCityFlightData;
-import si.nerve.flightchecker.data.PriceType;
-import si.nerve.flightchecker.helper.Helper;
 
 /**
  * Created: 10.8.13 20:39
@@ -91,14 +88,15 @@ public class EdreamsFlightObtainer implements MultiCityFlightObtainer
       ins = new GZIPInputStream(ins);
     }
 
-    //String response = Helper.readResponse(ins, getCharSetFromConnection(connection));
+//    String response = Helper.readResponse(ins, getCharSetFromConnection(connection));
     Map<String, List<String>> headerFields = connection.getHeaderFields();
-    url = new URL(address);
+    String newAddress = hostAddress + "/engine/ItinerarySearch/search";
+    url = new URL(newAddress);
     connection = createHttpConnection(url);
     StringBuilder sbuf = new StringBuilder();
     for (String cookie : headerFields.get("Set-Cookie"))
     {
-      if (sbuf.length() > 0)
+      if (sbuf.length() > 0 && !sbuf.toString().trim().endsWith(";"))
       {
         sbuf.append("; ");
       }
@@ -108,6 +106,8 @@ public class EdreamsFlightObtainer implements MultiCityFlightObtainer
     connection.addRequestProperty("Host", "www.edreams." + addressRoot);
     connection.addRequestProperty("Origin", "http://www.edreams." + addressRoot);
     connection.addRequestProperty("Referer", address);
+    connection.addRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+    connection.setDoInput(true);
     connection.setDoOutput(true);
     connection.setInstanceFollowRedirects(false);
     connection.setRequestMethod("POST");
@@ -140,21 +140,11 @@ public class EdreamsFlightObtainer implements MultiCityFlightObtainer
     data.put("collectionTypeEstimationNeeded", "false");
     data.put("applyAllTaxes", "false");
 
-    DataOutputStream out = new DataOutputStream(connection.getOutputStream());
+    String postString = getPostFormData(data);
+    connection.addRequestProperty("Content-Length", String.valueOf(postString.length()));
 
-    Set keys = data.keySet();
-    Iterator keyIter = keys.iterator();
-    String content = "";
-    for (int i = 0; keyIter.hasNext(); i++)
-    {
-      Object key = keyIter.next();
-      if (i != 0)
-      {
-        content += "&";
-      }
-      content += key + "=" + URLEncoder.encode(data.get(key), "UTF-8");
-    }
-    out.writeBytes(content);
+    DataOutputStream out = new DataOutputStream(connection.getOutputStream());
+    out.writeBytes(postString);
     out.flush();
     out.close();
 
@@ -166,8 +156,144 @@ public class EdreamsFlightObtainer implements MultiCityFlightObtainer
     }
 
     String response = Helper.readResponse(ins, getCharSetFromConnection(connection));
-    int streamingStartLocation = response.indexOf("Flight-SearchResults");
-    //int streamingEndLocation = response.indexOf("';", streamingStartLocation);
+    if (response.contains("singleItineray-content-body"))
+    {
+      for (int i = 2; i < 10; i++)
+      {
+        Document doc = Jsoup.parse(response);
+        Elements links = doc.select("div[class^=singleItineray-content-body]");
+        List<MultiCityFlightData> returnList = new ArrayList<MultiCityFlightData>();
+        for (Element link : links)
+        {
+          String price = null, priceNumber = null;
+          for (Node node : link.select("div[class^=singleItinerayPrice]").first().childNodes())
+          {
+            if (node instanceof TextNode && ((TextNode) node).text().trim().length() > 0)
+            {
+              price = link.select("a[class^=results_price]").text();
+              priceNumber = price.replaceAll("[\\D]", "");
+            }
+          }
+          Elements legs = link.select("td.singleItineray-content-flights table[id^=tableItineraryContent]");
+          LinkedList<FlightLeg> legList = new LinkedList<FlightLeg>();
+          int j = 1;
+          for (Element leg : legs)
+          {
+
+//            legList.add(new FlightLeg(
+//                from1,
+//                airportFromCode,
+//                localFormatter.format(departureTime),
+//                airportToName,
+//                airportToCode,
+//                localFormatter.format(arrivalTime),
+//                duration,
+//                layover
+//            ));
+          }
+//          String localeTotalPrice = (String) flight.get("localeTotalPrice");
+//          JSONArray legs = (JSONArray) flight.get("legs");
+//          for (Object leg1 : legs)
+//          {
+//            JSONObject leg = (JSONObject) leg1;
+//            JSONObject departureAirport = (JSONObject) leg.get("departureAirport");
+//            String airportFromCode = (String) departureAirport.get("airportCode");
+//            String airportFromName = (String) departureAirport.get("airportName");
+//            Date departureTime = formatter.parse((String) leg.get("departureTime"));
+//            JSONObject arrivalAirport = (JSONObject) leg.get("arrivalAirport");
+//            String airportToCode = (String) arrivalAirport.get("airportCode");
+//            String airportToName = (String) arrivalAirport.get("airportName");
+//            Date arrivalTime = formatter.parse((String) leg.get("arrivalTime"));
+//            String duration = getDuration((JSONObject) leg.get("totalTravelingHours"));
+//            String layover = getLayover((JSONArray) leg.get("segments"));
+//            legList.add(new FlightLeg(
+//                airportFromName,
+//                airportFromCode,
+//                localFormatter.format(departureTime),
+//                airportToName,
+//                airportToCode,
+//                localFormatter.format(arrivalTime),
+//                duration,
+//                layover
+//            ));
+//          }
+
+          if (legList.size() > 1)
+          {
+//            String priceNumber = localeTotalPrice.replaceAll("[\\D]", "");
+//            MultiCityFlightData flightData = new MultiCityFlightData(
+//                0,
+//                (String) flight.get("airFareBasisCode"),
+//                Integer.parseInt(priceNumber),
+//                PriceType.getInstance(localeTotalPrice),
+//                legList,
+//                "Tickets left: " + flight.get("noOfTicketsLeft"),
+//                address
+//            );
+//            returnList.add(flightData);
+//            statusLabel.setForeground(statusLabel.getForeground().equals(Color.BLACK) ? Color.DARK_GRAY : Color.BLACK);
+          }
+
+          synchronized (flightGui.getFlightQueue())
+          {
+            for (MultiCityFlightData flightData : returnList)
+            {
+              if (!flightGui.getFlightQueue().contains(flightData))
+              {
+                flightGui.getFlightQueue().add(flightData);
+
+                if (flightGui.getFlightQueue().size() > FlightsGui.QUEUE_SIZE)
+                {
+                  flightGui.getFlightQueue().remove();
+                }
+              }
+            }
+
+            if (returnList.size() > 0)
+            {
+              MultiCityFlightTableModel tableModel = (MultiCityFlightTableModel) flightGui.getMainTable().getModel();
+              tableModel.setEntityList(new ArrayList<MultiCityFlightData>(flightGui.getFlightQueue()));
+              tableModel.fireTableDataChanged();
+            }
+          }
+        }
+
+        newAddress = hostAddress + "/engine/ItinerarySearch/paging?page=" + i + "&isSEM=";
+        url = new URL(newAddress);
+        connection = createHttpConnection(url);
+        sbuf = new StringBuilder();
+        for (String cookie : headerFields.get("Set-Cookie"))
+        {
+          if (sbuf.length() > 0 && !sbuf.toString().trim().endsWith(";"))
+          {
+            sbuf.append("; ");
+          }
+          sbuf.append(cookie);
+        }
+        connection.addRequestProperty("Cookie", sbuf.toString());
+        ins = connection.getInputStream();
+        encoding = connection.getHeaderField("Content-Encoding");
+        if (encoding.equals("gzip"))
+        {
+          ins = new GZIPInputStream(ins);
+        }
+        response = Helper.readResponse(ins, getCharSetFromConnection(connection));
+        if (!response.contains("singleItineray-content-body"))
+        {
+          break;
+        }
+      }
+    }
+    else
+    {
+      int noResultsStartLocation = response.indexOf("no results available");
+      if (noResultsStartLocation > -1)
+      {
+        // AHA! no results
+      }
+    }
+
+    System.out.println();
     //try
     //{
     //  address = hostAddress + response.substring(streamingStartLocation, streamingEndLocation);
@@ -218,83 +344,30 @@ public class EdreamsFlightObtainer implements MultiCityFlightObtainer
     //
     //  Object obj = JSONValue.parse(jsonString);
     //  JSONArray array = (JSONArray)obj;
-    //  List<MultiCityFlightData> returnList = new ArrayList<MultiCityFlightData>();
-    //  for (Object anArray : array)
-    //  {
-    //    JSONObject flight = (JSONObject)anArray;
-    //    String localeTotalPrice = (String)flight.get("localeTotalPrice");
-    //    JSONArray legs = (JSONArray)flight.get("legs");
-    //    LinkedList<FlightLeg> legList = new LinkedList<FlightLeg>();
-    //    for (Object leg1 : legs)
-    //    {
-    //      JSONObject leg = (JSONObject)leg1;
-    //      JSONObject departureAirport = (JSONObject)leg.get("departureAirport");
-    //      String airportFromCode = (String)departureAirport.get("airportCode");
-    //      String airportFromName = (String)departureAirport.get("airportName");
-    //      Date departureTime = formatter.parse((String)leg.get("departureTime"));
-    //      JSONObject arrivalAirport = (JSONObject)leg.get("arrivalAirport");
-    //      String airportToCode = (String)arrivalAirport.get("airportCode");
-    //      String airportToName = (String)arrivalAirport.get("airportName");
-    //      Date arrivalTime = formatter.parse((String)leg.get("arrivalTime"));
-    //      String duration = getDuration((JSONObject)leg.get("totalTravelingHours"));
-    //      String layover = getLayover((JSONArray)leg.get("segments"));
-    //      legList.add(new FlightLeg(
-    //          airportFromName,
-    //          airportFromCode,
-    //          localFormatter.format(departureTime),
-    //          airportToName,
-    //          airportToCode,
-    //          localFormatter.format(arrivalTime),
-    //          duration,
-    //          layover
-    //      ));
-    //    }
-    //
-    //    if (legList.size() > 1)
-    //    {
-    //      String priceNumber = localeTotalPrice.replaceAll("[\\D]", "");
-    //      MultiCityFlightData flightData = new MultiCityFlightData(
-    //          0,
-    //          (String)flight.get("airFareBasisCode"),
-    //          Integer.parseInt(priceNumber),
-    //          PriceType.getInstance(localeTotalPrice),
-    //          legList,
-    //          "Tickets left: " + flight.get("noOfTicketsLeft"),
-    //          address
-    //      );
-    //      returnList.add(flightData);
-    //      statusLabel.setForeground(statusLabel.getForeground().equals(Color.BLACK) ? Color.DARK_GRAY : Color.BLACK);
-    //    }
-    //  }
-    //
-    //  synchronized (flightGui.getFlightQueue())
-    //  {
-    //    for (MultiCityFlightData flightData : returnList)
-    //    {
-    //      if (!flightGui.getFlightQueue().contains(flightData))
-    //      {
-    //        flightGui.getFlightQueue().add(flightData);
-    //
-    //        if (flightGui.getFlightQueue().size() > FlightsGui.QUEUE_SIZE)
-    //        {
-    //          flightGui.getFlightQueue().remove();
-    //        }
-    //      }
-    //    }
-    //
-    //    if (returnList.size() > 0)
-    //    {
-    //      MultiCityFlightTableModel tableModel = (MultiCityFlightTableModel)flightGui.getMainTable().getModel();
-    //      tableModel.setEntityList(new ArrayList<MultiCityFlightData>(flightGui.getFlightQueue()));
-    //      tableModel.fireTableDataChanged();
-    //    }
-    //  }
+
     //}
     //catch (Exception e)
     //{
     //  LOG.error("ExpediaFlightObtainer: JSON decoding failed!", e);
     //  statusLabel.setForeground(Color.RED);
     //}
+  }
+
+  private String getPostFormData(Map<String, String> data) throws UnsupportedEncodingException
+  {
+    Set keys = data.keySet();
+    Iterator keyIter = keys.iterator();
+    StringBuilder builder = new StringBuilder();
+    for (int i = 0; keyIter.hasNext(); i++)
+    {
+      Object key = keyIter.next();
+      if (i != 0)
+      {
+        builder.append('&');
+      }
+      builder.append(key).append('=').append(URLEncoder.encode(data.get(key), "UTF-8"));
+    }
+    return builder.toString();
   }
 
   private String getLayover(JSONArray segments)
@@ -313,16 +386,16 @@ public class EdreamsFlightObtainer implements MultiCityFlightObtainer
         {
           builder.append(",");
         }
-        JSONObject segment = (JSONObject)segments.get(i);
-        JSONObject layover = (JSONObject)segment.get("layover");
+        JSONObject segment = (JSONObject) segments.get(i);
+        JSONObject layover = (JSONObject) segment.get("layover");
         if (layover != null)
         {
-          days += (Long)layover.get("numOfDays");
-          hours += (Long)layover.get("hours");
-          minutes += (Long)layover.get("minutes");
+          days += (Long) layover.get("numOfDays");
+          hours += (Long) layover.get("hours");
+          minutes += (Long) layover.get("minutes");
         }
-        JSONObject arrivalAirport = (JSONObject)segment.get("arrivalAirport");
-        builder.append((String)arrivalAirport.get("airportCode"));
+        JSONObject arrivalAirport = (JSONObject) segment.get("arrivalAirport");
+        builder.append((String) arrivalAirport.get("airportCode"));
       }
       builder.append(" (");
       if (days > 0)
@@ -336,9 +409,9 @@ public class EdreamsFlightObtainer implements MultiCityFlightObtainer
 
   private String getDuration(JSONObject totalTravelingHours)
   {
-    Long numOfDays = (Long)totalTravelingHours.get("numOfDays");
-    Long hours = (Long)totalTravelingHours.get("hours");
-    Long minutes = (Long)totalTravelingHours.get("minutes");
+    Long numOfDays = (Long) totalTravelingHours.get("numOfDays");
+    Long hours = (Long) totalTravelingHours.get("hours");
+    Long minutes = (Long) totalTravelingHours.get("minutes");
     if (0 == numOfDays)
     {
       if (0 == hours)
@@ -384,14 +457,14 @@ public class EdreamsFlightObtainer implements MultiCityFlightObtainer
   {
     URLConnection connection = url.openConnection();
     connection.addRequestProperty("Connection", "keep-alive");
-        connection.addRequestProperty("Cache-Control", "max-age=0");
+    connection.addRequestProperty("Cache-Control", "max-age=0");
     connection.addRequestProperty("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
     connection.addRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.95 Safari/537.36");
     connection.addRequestProperty("Host", url.getHost());
     connection.addRequestProperty("Referer", url.toString());
     connection.addRequestProperty("Accept-Encoding", "gzip,deflate,sdch");
     connection.addRequestProperty("Accept-Language", "en-US,en;q=0.8");
-    return (HttpURLConnection)connection;
+    return (HttpURLConnection) connection;
   }
 
   public static void main(String[] args)
@@ -400,7 +473,8 @@ public class EdreamsFlightObtainer implements MultiCityFlightObtainer
     SimpleDateFormat formatter = new SimpleDateFormat("dd.MM.yyyy");
     try
     {
-      obtainer.search(null, null, "com", "LJU", "NYC", formatter.parse("18.12.2013"), "NYC", "VIE", formatter.parse("07.01.2014"), "NYC", "VIE", formatter.parse("07.01.2014"), 1);
+      obtainer.search(null, null, "com", "VCE", "BKK", formatter.parse("07.10.2013"), "BKK", "VCE", formatter.parse("21.10.2013"), "VCE", "NYC", formatter.parse("31.10.2013"), 1);
+//      obtainer.search(null, null, "com", "LJU", "NYC", formatter.parse("18.12.2013"), "NYC", "VIE", formatter.parse("07.01.2014"), "NYC", "VIE", formatter.parse("07.01.2014"), 1);
     }
     catch (Exception e)
     {
